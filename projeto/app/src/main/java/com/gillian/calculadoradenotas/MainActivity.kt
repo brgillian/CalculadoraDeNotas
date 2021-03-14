718d9eac7a93e6a5dc040c669e15aa306d057042
brgillian <brgillian@gmail.com>
2021-03-14 17:42:55 -0300

    Funcao de limpar campos adicionada.

--- projeto/app/src/main/java/com/gillian/calculadoradenotas/MainActivity.kt
+++ projeto/app/src/main/java/com/gillian/calculadoradenotas/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gillian.calculadoradenotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.gillian.calculadoradenotas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +14,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val num1 = binding.nota1
+        val num2 = binding.nota2
+        val num3 = binding.nota3
+        val num4 = binding.nota4
+        val nfaltas = binding.faltas
+        val resultado = binding.txtResultado
+
         binding.btCalcular.setOnClickListener{
-            val num1 = binding.nota1
+           /* val num1 = binding.nota1
             val num2 = binding.nota2
             val num3 = binding.nota3
             val num4 = binding.nota4
             val nfaltas = binding.faltas
-            val resultado = binding.txtResultado
+            val resultado = binding.txtResultado*/
 
             val nota1 = Integer.parseInt(num1.text.toString())
             val nota2 = Integer.parseInt(num2.text.toString())
@@ -39,7 +47,14 @@ class MainActivity : AppCompatActivity() {
                 resultado.setText("O aluno foi reprovado por nota. \n Média final: $media")
                 resultado.setTextColor(getColor(R.color.vermelho))
             }
-
+        }
+        binding.btLimpar.setOnClickListener{
+            num1.setText("")
+            num2.setText("")
+            num3.setText("")
+            num4.setText("")
+            nfaltas.setText("")
+            resultado.setText("")
         }
     }
 }
